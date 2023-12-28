f4d92c39b573e7f8647df803c122e5b809b45146
arpan-06 <arpanmukherjee06@gmail.com>
2023-12-28 15:34:36 +0530

    new code added

--- Callback/Index.js
+++ Callback/Index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const cart= ["shoes","pant","kurta"];
+
+api.createOrder(cart,function(){
+
+    api.proccedToPayment(function(){
+
+        api.updateWalet(function(){
+            
+            viewOrderSummary();
+        })
+    });
+});
